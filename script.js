e40ddcbc5d5c0fedc607e20e34f33f5cc570e07b
sam99-git <82113325+sam99-git@users.noreply.github.com>
2023-05-18 12:31:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 $(document).ready(function(){
-    var url ="https://api.covid19india.org/data.json"
+    var url ="https://data.covid19india.org/data.json"
     $.getJSON(url ,function(data){
         console.log(data)
-
         var total_active,total_recovered,total_deaths,total_confirmed
-
         var state = []
         var confirmed = []
         var recovered = []
         var deaths = []
-
-
+        
         $.each(data.statewise,function(id,obj){
             state.push(obj.state)
             confirmed.push(obj.confirmed)
             recovered.push(obj.recovered)
             deaths.push(obj.deaths)
-
         })
 
         state.shift()
